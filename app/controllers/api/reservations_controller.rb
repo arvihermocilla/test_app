ca415b3b5f03297f21c5551eeae489350516a7bd
Arvi Hermocilla <arvihermocilla@gmail.com>
2025-01-29 13:20:27 +0800

    update controller to add guard incase creation of guest fails

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -2,10 +2,14 @@ module Api
   class ReservationsController < ApplicationController
     def create
       form = ReservationForm.new(params.permit!)
-      guest = Guest.create(form.attributes[:guest_attr])
-      guest.reservations.create(form.attributes[:reservation_attr])
+      guest = Guest.new(form.attributes[:guest_attr])
 
-      render json: { message: "Reservation created successfully" }, status: 200
+      if guest.save
+        guest.reservations.create!(form.attributes[:reservation_attr])
+        render json: { message: "Reservation created successfully" }, status: :ok
+      else
+        render json: { errors: guest.errors.full_messages }, status: :unprocessable_entity
+      end
     end
   end
 end
